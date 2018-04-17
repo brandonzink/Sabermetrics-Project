1e71c59a71e4fb4a66cf05f053c4668c252a3eb1
Brandon Zink <brandon.zink@colorado.edu>
2018-04-17 12:54:36 -0600

    Reorganized

--- Code/Queries.sql
+++ Code/Queries.sql
@@ -1,7 +1,5 @@
-select distinctrow e.GAME_ID, e.INN_CT, concat(ros.FIRST_NAME_TX,' ',ros.LAST_NAME_TX) as 'NAME', e.BAT_ID, substring(e.GAME_ID,4,4) as 'Year', e.BASE1_RUN_ID, e.BASE2_RUN_ID, e.BASE3_RUN_ID, e.BAT_DEST_ID, e.RUN1_DEST_ID, 
-e.RUN2_DEST_ID, e.RUN3_DEST_ID from events e
+select concat(ros.FIRST_NAME_TX,' ',ros.LAST_NAME_TX) as 'NAME', e.BAT_ID as 'Batter ID', substring(e.GAME_ID,4,4) as 'Year', e.OUTS_CT as 'Outs', e.BASE1_RUN_ID as '1B Runner', e.BASE2_RUN_ID as '2B Runner', e.BASE3_RUN_ID as '3B Runner', e.BAT_DEST_ID 'Batter Dest.', e.RUN1_DEST_ID '1B Runner Dest.', 
+e.RUN2_DEST_ID as '2B Runner Dest.', e.RUN3_DEST_ID as '3B Runner Dest', e.START_BASES_CD as 'Start Bases', e.END_BASES_CD as 'End Bases', 
+e.EVENT_RUNS_CT as 'Event Runs', e.EVENT_OUTS_CT as 'Event Outs' from events e
 join rosters ros on ros.PLAYER_ID = e.BAT_ID
-where ERR_CT = 0 and (e.BAT_DEST_ID != 0 or e.RUN1_DEST_ID != 0 or e.RUN2_DEST_ID != 0 or e.RUN3_DEST_ID != 0)
-group by e.BAT_ID; #and substring(e.GAME_ID,4,4));
-
-select * from events;
+where ERR_CT = 0 and (e.BAT_DEST_ID != 0 or e.RUN1_DEST_ID != 0 or e.RUN2_DEST_ID != 0 or e.RUN3_DEST_ID != 0);
